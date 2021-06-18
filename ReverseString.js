ab12bf9c08d28b60c9d14be29c987480f582a0c1
Fadimata Tillman <fadi.tillman@gmail.com>
2021-06-18 12:39:41 -0700

    ReverseString.js solution

---
+++ ReverseString.js
@@ -0,0 +1,16 @@
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+ 
+
+// Example 1:
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+
+
+var reverseString = function(s) {
+  
+    // return reverse.
+    s = s.reverse()
+    return s
+};
