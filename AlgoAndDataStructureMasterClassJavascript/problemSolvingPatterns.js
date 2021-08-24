86442100e2352bc2a150a5aeef62fff6efcc56c2
Fadimata Tillman <fadi.tillman@gmail.com>
2021-08-24 12:37:30 -0700

    unique value solution

--- AlgoAndDataStructureMasterClassJavascript/problemSolvingPatterns.js
+++ AlgoAndDataStructureMasterClassJavascript/problemSolvingPatterns.js
@@ -158,3 +158,29 @@ while(left < right){
     }
   }
 }
+
+//multiple pointers: count unique value
+//Implement a function called countUniqueValues, wich accepts a sorted array, and counts the 
+//unique values in the array. There can be negative  numbers in the array, but it will be sorted.
+
+//Example 
+ countUniqueValues([1,1,1,1,1,2]) //2
+ countUniqueValues([]) //0
+
+ //brut force 0(n)
+ //1 loop
+ function countUniqueValues(arr){
+    if( arr.length === 0){
+   return 0
+    }
+    var i = 0;
+     for(var j = 1; j < arr.length; j++){
+     if(arr[i]!== arr[j]){
+       i++;
+       arr[i] = arr[j]
+      }
+     }
+    return i+1
+  }
+   countUniqueValues([[1,1,1,1,1,2]])
+
