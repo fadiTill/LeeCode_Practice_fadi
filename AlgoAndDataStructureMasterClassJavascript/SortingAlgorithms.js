817c72b138d4e2f0af8e8cdaa457990c2c76c632
Fadimata Tillman <fadi.tillman@gmail.com>
2021-09-14 10:49:24 -0700

    SelectionSort

--- AlgoAndDataStructureMasterClassJavascript/SortingAlgorithms.js
+++ AlgoAndDataStructureMasterClassJavascript/SortingAlgorithms.js
@@ -128,3 +128,64 @@ function bubbleSort(arr){
 bubbleSort([8,1,2,3,4,5,6,7]);
 
 //time complexity in general is 0(n2) because of nested loop
+
+
+
+//Selection Sort
+
+//place small values into sorted position.
+//make a variable to store min value 
+//compare to the next item  until found smaller num
+//if  small num found set to be new min and iterate until end of array.
+//if minim is not index you start with  swap the two values.
+
+
+function selectionSort (arr){
+    for (var i = 0; i < arr.length; i++){
+      var min = i;
+      for (var j = i+1; j < arr.length; j++ ){
+        if (arr[j] < arr[min]){
+          min = j;
+        }
+      }
+      // if (i !== min){
+        // console.log("******")
+        // console.log(arr)
+        // console.log("Swaping")
+        console.log(i,min)
+      var temp = arr[i]
+      arr[i] = arr[min]
+      arr[min] = temp
+      // }
+      
+    }
+    return arr;
+  }
+  
+  selectionSort([0,2,34,22,10,19,17])
+//[34,22,10,19,17]
+//i,j,min
+//0,1,1
+//0,2,2
+//0,3,2
+//0,4,2
+
+//ES615
+function selectionSort(arr) {
+    const swap = (arr, idx1, idx2) =>
+    ([arr[idx1], arr[idx2]]= [arr[idx2],arr[idx1]])
+  
+    for(let i = 0; i < arr.length; i++){
+      let min = i;
+      for (let j = i+1; j < arr.length; j++){
+        if(arr[min]> arr[j]){
+        min = j;
+      }
+    }
+    if (i!== min) swap(arr,i,min);
+  }
+  return arr;
+  }
+   selectionSort([0,2,34,22,10,19,17])
+
+   //time complexity O(n^2)
