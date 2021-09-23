610a0bd411c5fb2b40e0bb954f888e9f6474f510
Fadimata Tillman <fadi.tillman@gmail.com>
2021-09-23 10:04:15 -0700

    quick sort part2

--- AlgoAndDataStructureMasterClassJavascript/quickSort.js
+++ AlgoAndDataStructureMasterClassJavascript/quickSort.js
@@ -8,24 +8,38 @@
 
 //function helper
 function pivot(arr, start=0, end=arr.length+1){
-   function swap(array, i, j){
-       var temp = array[i];
-       array[i] = array[j]
-       array[j] = temp;
-   }
-
-    var pivot = arr[star];
-    var swapIdx = start;
-
-    for(var i = start + 1; i < arr.length; i++){
-        if(pivot > arr[i]){
-            swapIdx++;
-            swap(arr, swapIdx, i)
-
-        }
+    function swap(array, i, j){
+        var temp = array[i];
+        array[i] = array[j]
+        array[j] = temp;
     }
-   swap(arr,start,swaIdx)
-   return swapIdx;
-}
+ 
+     var pivot = arr[start];
+     var swapIdx = start;
+ 
+     for(var i = start + 1; i < arr.length; i++){
+         if(pivot > arr[i]){
+             swapIdx++;
+             swap(arr, swapIdx, i)
+ 
+         }
+     }
+    swap(arr,start,swapIdx)
+    return swapIdx;
+ }
+ // pivot([4,8,2,1,5,7,6,3])
+ 
+ function quickSort(arr, left=0, right=arr.length-1 ){
+   if(left < right){
+     let pivotIndex = pivot(arr,left, right)
+   //left
+   quickSort(arr,left,pivotIndex-1);
+   //right
+   quickSort(arr,pivotIndex+1, right)
+ 
+   }
+   return arr;
+   
+ }
+ quickSort([4,6,9,1,2,5])
 
-pivot([4,8,2,1,5,7,6,3])
