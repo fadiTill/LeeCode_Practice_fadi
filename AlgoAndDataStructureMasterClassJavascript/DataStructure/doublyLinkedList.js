e52d62f2785cb768a2e07227bc5c292641b34e47
Fadimata Tillman <fadi.tillman@gmail.com>
2021-10-26 11:34:24 -0700

    pop review

--- AlgoAndDataStructureMasterClassJavascript/DataStructure/doublyLinkedList.js
+++ AlgoAndDataStructureMasterClassJavascript/DataStructure/doublyLinkedList.js
@@ -28,7 +28,7 @@ if(this.length === 0){
 //if not, set the next property on the tail to be that node 
 } else {
     this.tail.next = newNode;
-    this.newNode.prev = this.tail;
+    newNode.prev = this.tail;
     this.tail = newNode;
     //set the previous property on the newly created node to be the tail.
 //set the tail property to be the  newly created node 
@@ -41,7 +41,37 @@ if(this.length === 0){
 this.length++;
 return this;
 }
+pop(){
+    //if there is no head return undefined 
+    if(!this.head) return undefined;
+    //store the current tail in a avariable to return later
+    var poppedNode = this.tail;
+    if (this.length === 1){
+    this.head = null;
+    this.tail = null;
+
+    //if the length is 1, set the head and tail to be null
+
+     } else {
+        //update the tail to be the previous Node.
+    this.tail = poppedNode.prev;
+    //Set newTail's next to null
+    this.tail.next = null;
+    poppedNode.prev = null;
+
+      }
+      //decrement
+      this.length--;
+      //return the value removed 
+      return poppedNode
+  }
 }
 list =  new DoublyLinkedList;
 console.log(list)
 list.pushDoubly(99)
+
+
+
+
+
+
