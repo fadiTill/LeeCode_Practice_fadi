97eff801d84b9467b923048ef119df2edb33b9fa
Fadimata Tillman <fadi.tillman@gmail.com>
2021-11-19 09:20:16 -0800

    review

---
+++ AlgoAndDataStructureMasterClassJavascript/reviewjs/reviewBis.js
@@ -0,0 +1,69 @@
+// if(2 !== 1){
+//     console.log('IT IS TRUE')
+// }
+
+
+// let rating = 0;
+// if(rating === 3 ) {
+//     console.log('you are are super start')
+
+// }
+// else if (rating === 2 ){
+//     console.log("meet expectation")
+// }
+
+// else if (rating === 1 ){
+//     console.log('Needs improvement');
+// }
+
+// else {
+//   console.log('invalide')  
+// }
+
+
+// let highScore = 1430;
+// let userScore = 1200;
+
+// if(userScore >= highScore){
+//     console.log(`congrats  your ${userScore}`);
+//     highScore = userScore;
+// }
+// else{
+//    console.log(
+//        `your score ${userScore} did not bit ${highScore}`
+//        ) 
+// }
+
+// let password = 'hellofadi';
+
+// if(password.length >= 6){
+//     if(password.indexOf(' ') === -1) {
+//         console.log('valid')
+//     }
+//     else {
+//         console.log('Password is long but cannot contain spaces');
+//     }
+// }
+
+// else {
+//     console.log('password must be longer!');
+// }
+
+// let password ="chickenGal";
+
+// if(password.length >= 8 && password.indexOf('') === -1){
+//     console.log("Valid")
+// } else {
+//     console.log("invalid")
+// }
+
+// let num = 11;
+
+// if(num >= 1 && num < 10){
+//     console.log(`Number`)
+// }
+// else{
+//     console.log(`please guess number between 1 and 10`)
+// }
+
+let colors = ['RED', 'ORANGE','BLUE'];
