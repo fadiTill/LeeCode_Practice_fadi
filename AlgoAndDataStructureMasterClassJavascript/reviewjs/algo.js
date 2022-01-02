99e5299b1046aeea282b5aba2e90419a206f64d4
Fadimata Tillman <fadi.tillman@gmail.com>
2022-01-02 14:39:46 -0800

    practice

--- AlgoAndDataStructureMasterClassJavascript/reviewjs/algo.js
+++ AlgoAndDataStructureMasterClassJavascript/reviewjs/algo.js
@@ -1,10 +1,22 @@
-let instructor = {
-    firstnName: "Kelly",
-    isInstructor: true,
-    favaoriteNumbers: [1,2,3,4]
-  }
+// let instructor = {
+//     firstnName: "Kelly",
+//     isInstructor: true,
+//     favaoriteNumbers: [1,2,3,4]
+//   }
   
   
-  Object.keys(instructor)
-  Object.entries(instructor)
-  instructor.hasOwnProperty('firstnName')
+//   Object.keys(instructor)
+//   Object.entries(instructor)
+//   instructor.hasOwnProperty('firstnName')
+
+
+
+let names = ["Michael", "Melissa", "Andrea"]
+names.push()
+names.pop()
+names.shift()
+name.unshift()
+names.concat()
+names.slice()
+names.splice()
+names.sort()
