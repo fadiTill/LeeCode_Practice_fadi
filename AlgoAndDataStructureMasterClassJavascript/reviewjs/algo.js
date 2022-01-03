ed50faf6bd497763c021ba85d1c23749f80226a3
Fadimata Tillman <fadi.tillman@gmail.com>
2022-01-02 16:10:18 -0800

    practice same squared

--- AlgoAndDataStructureMasterClassJavascript/reviewjs/algo.js
+++ AlgoAndDataStructureMasterClassJavascript/reviewjs/algo.js
@@ -11,12 +11,33 @@
 
 
 
-let names = ["Michael", "Melissa", "Andrea"]
-names.push()
-names.pop()
-names.shift()
-name.unshift()
-names.concat()
-names.slice()
-names.splice()
-names.sort()
+// let names = ["Michael", "Melissa", "Andrea"]
+// names.push()
+// names.pop()
+// names.shift()
+// name.unshift()
+// names.concat()
+// names.slice()
+// names.splice()
+// names.sort()
+
+//Frequency counter
+
+
+
+function same(array1, array2){
+    if(array1.length !== array2.length){
+        return false;
+    } 
+    for (let i = 0; i < array1.length; i++){
+        let corectIndex = array2.indexOf((array1[i]**2))
+          if(corectIndex === -1){ 
+              return false;  
+         }
+         console.log(array2)
+      array2.splice(corectIndex, 1)
+    }
+    return true;
+}
+
+same([1,2,3,2], [9,1,4,4])
