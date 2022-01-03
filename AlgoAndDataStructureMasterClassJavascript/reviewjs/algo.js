3c8904fe8bebae92ce942f4046331f999907cfd5
Fadimata Tillman <fadi.tillman@gmail.com>
2022-01-02 18:52:08 -0800

    value algo

--- AlgoAndDataStructureMasterClassJavascript/reviewjs/algo.js
+++ AlgoAndDataStructureMasterClassJavascript/reviewjs/algo.js
@@ -114,19 +114,37 @@
 // sum([-4,-3,-2,-1,0,1,2,5])
 
 
-function sum(arr){
-    let left = 0;
-    let right = arr.length-1;
-    while(left < right){
-        let sum = arr[left] + arr[right];
-        if(sum = 0){
-            return [arr[left], arr[right]];
-        } else if (sum > 0){
-            right--;
-        } else {
-            left++;
-        }
+// function sum(arr){
+//     let left = 0;
+//     let right = arr.length-1;
+//     while(left < right){
+//         let sum = arr[left] + arr[right];
+//         if(sum = 0){
+//             return [arr[left], arr[right]];
+//         } else if (sum > 0){
+//             right--;
+//         } else {
+//             left++;
+//         }
 
+//     }
+// }
+// sum([-4,-3,-2,-1,0,1,2,5])
+
+
+function countUniqueValues(arr){
+    if( arr.length === 0){
+   return 0;
     }
-}
-sum([-4,-3,-2,-1,0,1,2,5])
+    var i = 0;
+     for(var j = 1; j < arr.length; j++){
+     if(arr[i]!== arr[j]){
+       i++;
+       arr[i] = arr[j];
+      }
+     }
+    return i+1;
+  }
+  
+
+  countUniqueValues([1,1,1,2])
