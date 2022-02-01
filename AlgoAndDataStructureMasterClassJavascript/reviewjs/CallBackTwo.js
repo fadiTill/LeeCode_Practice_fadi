03dcaecf09acde8585e395111c3ed4bfa1608527
Fadimata Tillman <fadi.tillman@gmail.com>
2022-02-01 14:12:16 -0800

    test

---
+++ AlgoAndDataStructureMasterClassJavascript/reviewjs/CallBackTwo.js
@@ -0,0 +1,74 @@
+const moveX = (element, amount, delay, onSuccess, onFailure) => {
+	setTimeout(() => {
+		const bodyBoundary = document.body.clientWidth;
+		const elRight = element.getBoundingClientRect().right;
+		const currLeft = element.getBoundingClientRect().left;
+		if (elRight + amount > bodyBoundary) {
+			onFailure();
+		}
+		else {
+			element.style.transform = `translateX(${currLeft + amount}px)`;
+			onSuccess();
+		}
+	}, delay);
+};moveX(
+	btn,
+	300,
+	1000,
+	() => {
+		//success callback
+		moveX(
+			btn,
+			300,
+			1000,
+			() => {
+				//success callback
+				moveX(
+					btn,
+					300,
+					1000,
+					() => {
+						//success callback
+						moveX(
+							btn,
+							300,
+							1000,
+							() => {
+								//success callback
+								moveX(
+									btn,
+									300,
+									1000,
+									() => {
+										//success callback
+										alert('YOU HAVE A WIDE SCREEN!');
+									},
+									() => {
+										//failure callback
+										alert('CANNOT MOVE FURTHER!');
+									}
+								);
+							},
+							() => {
+								//failure callback
+								alert('CANNOT MOVE FURTHER!');
+							}
+						);
+					},
+					() => {
+						//failure callback
+						alert('CANNOT MOVE FURTHER!');
+					}
+				);
+			},
+			() => {
+				//failure callback
+				alert('CANNOT MOVE FURTHER!');
+			}
+		);
+	},
+	() => {
+		//failure callback
+		alert('CANNOT MOVE FURTHER!');
+	}
+);
---
+++ AlgoAndDataStructureMasterClassJavascript/reviewjs/promisesJStwo.js
@@ -0,0 +1,57 @@
+const willGetYouADog = new Promise((resolve, reject) => {
+	const rand = Math.random();
+	if (rand < 0.5) {
+		resolve();
+	}
+	else {
+		reject();
+	}
+});
+willGetYouADog.then(() => {
+	console.log('YAY WE GOT A DOG!!!!');
+});
+willGetYouADog.catch(() => {
+	console.log(':( NO DOG');
+});
+
+
+
+const moveX = (element, amount, delay) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const bodyBoundary = document.body.clientWidth;
+			const elRight = element.getBoundingClientRect().right;
+			const currLeft = element.getBoundingClientRect().left;
+			if (elRight + amount > bodyBoundary) {
+				reject({ bodyBoundary, elRight, amount });
+			}
+			else {
+				element.style.transform = `translateX(${currLeft + amount}px)`;
+				resolve();
+			}
+		}, delay);
+	});
+};
+
+const btn = document.querySelector('button');
+moveX(btn, 100, 1000)
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.catch(({ bodyBoundary, amount, elRight }) => {
+		console.log(`Cannot Move! Body is ${bodyBoundary}px wide`);
+		console.log(`Element is at ${elRight}px, ${amount}px is too large!`);
+	});
+
