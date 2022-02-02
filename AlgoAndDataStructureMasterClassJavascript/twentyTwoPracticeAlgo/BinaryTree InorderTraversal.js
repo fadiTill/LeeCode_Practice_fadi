c4f4d51f8bb35e5cd3ef4ad0b16145054ce7cfea
Fadimata Tillman <fadi.tillman@gmail.com>
2022-02-02 11:35:41 -0800

    100. Same Tree

---
+++ AlgoAndDataStructureMasterClassJavascript/twentyTwoPracticeAlgo/BinaryTree InorderTraversal.js
@@ -0,0 +1,32 @@
+// Binary Tree Inorder Traversal
+
+// Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+//  * @param {TreeNode} root
+//  * @return {number[]}
+ */
+ var inorderTraversal = function(root) {
+    console.log(root)
+  
+  let data = [];
+    function  traverse(node){
+    
+       if(node.left) traverse(node.left)
+          data.push(node.val)
+       if(node.right) traverse(node.right)
+      
+ }
+ traverse(root);
+     return data;
+    
+};
+    
