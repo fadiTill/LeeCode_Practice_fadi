8546fe70d26ae0170d546dd0f15350a102dad062
Fadimata Tillman <fadi.tillman@gmail.com>
2022-02-04 11:46:42 -0800

    104. Maximum Depth of Binary Tree

--- AlgoAndDataStructureMasterClassJavascript/twentyTwoPracticeAlgo/SymmetricTree.js
+++ AlgoAndDataStructureMasterClassJavascript/twentyTwoPracticeAlgo/SymmetricTree.js
@@ -8,3 +8,37 @@
 // Output: false
  
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+//  * @param {TreeNode} root
+//  * @return {boolean}
+//  */
+
+//traverse tree left and right side compare each l and R child if equal true else false 
+
+
+// Solution traverse tree left and right side compare each l and R child if equal true else false 
+
+
+var isSymmetric = function(root) {
+    
+            return isMirror(root.left, root.right);
+    }
+    
+    function isMirror( t1,  t2) {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null || t2 == null) return false;
+        return (t1.val == t2.val)
+            && isMirror(t1.right, t2.left)
+            && isMirror(t1.left, t2.right);
+        
+        
+    };
+    
