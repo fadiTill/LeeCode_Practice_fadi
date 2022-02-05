ddf617419fcf12fe89abe5661c3ef980a4f54ba4
Fadimata Tillman <fadi.tillman@gmail.com>
2022-02-05 14:17:59 -0800

    balanced practice

--- AlgoAndDataStructureMasterClassJavascript/twentyTwoPracticeAlgo/ balancedBinaryTree.js
+++ AlgoAndDataStructureMasterClassJavascript/twentyTwoPracticeAlgo/ balancedBinaryTree.js
@@ -1,49 +1,49 @@
 // 110. Balanced Binary Tree
 
 
-//solution fail egde case
-// var isBalanced = function(root) {
-//     if(root === null){
-//         return true
-//     }
-//     return height(root)
-// }
-//     let height = function(node){
-//         if(node === null){
-//             return true
-//         }
-    
-    
-//     let left = height(node.left)
-//     let right = height(node.right)
-    
-    
-//     if(Math.abs(height(node.left) - height(node.right)) > 1){
-//         return false 
-//     }else {
-//          return isBalanced(node.left) && isBalanced(node.right)
-//     }
-
-    
-// };
-
-
+// solution fail egde case
 var isBalanced = function(root) {
     if(root === null){
-        return true;
+        return true
     }
-    return height(root) !==-1;
+    return height(root)
 }
-
-let height = function(node){
-    if(node === null){
-        return 0;
-    }
-    let left = height(node.left);
-    let right = height(node.right);
+    let height = function(node){
+        if(node === null){
+            return true
+        }
     
-    if(left === -1 || right===-1 || Math.abs(left-right) >1 ){
-        return -1;
+    
+    let left = height(node.left)
+    let right = height(node.right)
+    
+    
+    if(Math.abs(height(node.left) - height(node.right)) > 1){
+        return false 
+    }else {
+         return isBalanced(node.left) && isBalanced(node.right)
     }
-    return Math.max(left, right)+1 ;
-}
+
+    
+ };
+
+
+// var isBalanced = function(root) {
+//     if(root === null){
+//         return true;
+//     }
+//     return height(root) !==-1;
+// }
+
+// let height = function(node){
+//     if(node === null){
+//         return 0;
+//     }
+//     let left = height(node.left);
+//     let right = height(node.right);
+    
+//     if(left === -1 || right===-1 || Math.abs(left-right) >1 ){
+//         return -1;
+//     }
+//     return Math.max(left, right)+1 ;
+// }
