122f049f5832fe30092a5c58c98c65c7e1496686
Fadimata Tillman <fadi.tillman@gmail.com>
2022-02-05 14:12:27 -0800

    110. Balanced Binary Tree practice

---
+++ AlgoAndDataStructureMasterClassJavascript/twentyTwoPracticeAlgo/ balancedBinaryTree.js
@@ -0,0 +1,49 @@
+// 110. Balanced Binary Tree
+
+
+//solution fail egde case
+// var isBalanced = function(root) {
+//     if(root === null){
+//         return true
+//     }
+//     return height(root)
+// }
+//     let height = function(node){
+//         if(node === null){
+//             return true
+//         }
+    
+    
+//     let left = height(node.left)
+//     let right = height(node.right)
+    
+    
+//     if(Math.abs(height(node.left) - height(node.right)) > 1){
+//         return false 
+//     }else {
+//          return isBalanced(node.left) && isBalanced(node.right)
+//     }
+
+    
+// };
+
+
+var isBalanced = function(root) {
+    if(root === null){
+        return true;
+    }
+    return height(root) !==-1;
+}
+
+let height = function(node){
+    if(node === null){
+        return 0;
+    }
+    let left = height(node.left);
+    let right = height(node.right);
+    
+    if(left === -1 || right===-1 || Math.abs(left-right) >1 ){
+        return -1;
+    }
+    return Math.max(left, right)+1 ;
+}
