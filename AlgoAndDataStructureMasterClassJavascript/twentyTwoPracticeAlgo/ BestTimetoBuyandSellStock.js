435ecc245a993a245b977aac2e81c5f727bbf85d
Fadimata Tillman <fadi.tillman@gmail.com>
2022-02-16 11:11:35 -0800

    121. Best Time to Buy and Sell Stock javascript solution

---
+++ AlgoAndDataStructureMasterClassJavascript/twentyTwoPracticeAlgo/ BestTimetoBuyandSellStock.js
@@ -0,0 +1,25 @@
+// 121. Best Time to Buy and Sell Stock
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+// example Input: prices = [7,1,5,3,6,4]
+// Output: 5
+
+
+var maxProfit = function(prices) {
+    let minprice = Number.MAX_VALUE
+    let maxprofit = 0;
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minprice)
+            minprice = prices[i];
+        else if (prices[i] - minprice > maxprofit)
+            maxprofit = prices[i] - minprice;
+    }
+    return maxprofit;
+
+};
