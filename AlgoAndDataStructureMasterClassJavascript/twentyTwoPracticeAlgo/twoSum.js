d1a6b9e6135cd94a27b81acdb9d3e04ac0f5e9d9
Fadimata Tillman <fadi.tillman@gmail.com>
2022-03-11 12:35:36 -0800

    two sum brut force javascript

---
+++ AlgoAndDataStructureMasterClassJavascript/twentyTwoPracticeAlgo/twoSum.js
@@ -0,0 +1,18 @@
+//brut force 
+
+
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+            for (let j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return [i,j];
+                }
+            }
+        }
+        // In case there is no solution, we'll just return null
+        return null;
+    
+
+    
+    
+};
