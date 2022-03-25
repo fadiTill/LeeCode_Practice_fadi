4089fb07561e10f56b23a8cf2c76a51359b8f734
Fadimata Tillman <fadi.tillman@gmail.com>
2022-03-25 15:42:50 -0700

    practice for loop

--- pyhon_practice_certificate/whileLoop.py
+++ pyhon_practice_certificate/whileLoop.py
@@ -1,7 +1,19 @@
 
+# def count_ten(num): 
+#  number = 1
+# while number < num:
+#      print("let's count", str(number))
+#      number = number+1
+#      print("number"+" "+str(number)+" "+"is after")
+# print(count_ten(10))
 
-number = 1
-while number < 10:
-     print("let's count", str(number))
-     number = number+1
-     print("number"+" "+str(number)+" "+"is after")
+# passcode = get_passcode()
+# while not valid_passcode(passcode):
+#     print(" you need to add a valid passcode")
+#     passcode = get_passcode 
+
+
+my_data = 7
+while my_data < 20:
+    print("Bonjour")
+    my_data +=2
