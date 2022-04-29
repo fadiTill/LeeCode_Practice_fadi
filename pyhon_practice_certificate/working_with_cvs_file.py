c91ca3f80d31a31fdd858428c77f292437139a2c
Fadimata Tillman <fadi.tillman@gmail.com>
2022-04-29 10:23:37 -0700

    generating csv file

--- pyhon_practice_certificate/working_with_cvs_file.py
+++ pyhon_practice_certificate/working_with_cvs_file.py
@@ -8,3 +8,14 @@ for row in csv_f:
     #unpaking name variables 
     print("Name:{},Phone:{},Role:{}".format(name,phone,role))
     f.close()
+
+
+    #generating csv file
+    #create a list
+    #machine name and ip adresse
+    hosts =[["workstation.local", "192.168.25.46"],["webserver.cloud","10.2.5.6"]]
+    #open file write mode 
+    with open('host.csv','w') as hosts_csv:
+        writer = csv.writer(hosts_csv)
+        writer.writerows(hosts)
+
