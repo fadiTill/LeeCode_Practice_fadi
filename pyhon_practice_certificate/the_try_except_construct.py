d37cf21cf41521425825c71d3af1a77d0dcc9618
Fadimata Tillman <fadi.tillman@gmail.com>
2022-05-18 12:06:43 -0700

    errors excpetions part2

--- pyhon_practice_certificate/the_try_except_construct.py
+++ pyhon_practice_certificate/the_try_except_construct.py
@@ -15,4 +15,53 @@ def character_frequency(filename):
             characteres[char] = characteres.gets(char, 0)+1
     f.close()
     return characteres
+
+
+
+    def validate_user(username, minlen):
+        assert type(username)== str, "username must be a string"
+        if minlen < 1:
+            raise ValueError("minlen must be at least 1")
+        if len(username) < minlen:
+            return False 
+        if not username.isalnum():
+            return False 
+        return True
+
+        #from validations import validate_user
+        #validate_user("", -1)
+        #......
+        #raise ValueError("minlen must be at least 1")
+        #validate_user("", 1)
+        #False
+        #validate_user("myuser",1)
+        #True
+
+
+
+# import unittest
+
+# from validations import validate_user
+
+# class TestValidateUser(unittest.TestCase):
+#     def test-valid(self):
+#         self.assertEqual(validate_user("validuser",3),True)
     
+#     def test_too_short(self):
+#         self.assertEqual(validate_user("inv",5),False)
+
+
+#         def test_invalid_charaters(self):
+#         self.assertEqual(validate-user("invalid_user",1),False)
+
+# def test-invalid_minlen(self):
+#     self.assertRaises(valueError, validate-user,"user", -1)
+
+#unittest.main()
+
+
+#./validation_test.py
+#.....
+#Ran 4 test in 0.000s
+
+#OK
