15265d9d9721dff2308d8fb32adf488d84099005
Fadimata Tillman <fadi.tillman@gmail.com>
2022-05-20 10:42:20 -0700

    intro bash

---
+++ pyhon_practice_certificate/intro_bash.py
@@ -0,0 +1,16 @@
+
+#convert 
+#mkdir mynewdir
+#cd mynewdir
+
+#pwd -> print current directory
+#      /home/user/mynewdir 
+
+# cp ../spider.txt .
+ # |   |           |
+  #copy     #parent dir   current dir
+
+ # touch  myfile.txt-> create empty file 
+ #ls -l -> total 4 -> -rw-r--r 1 user O Jan 8 14:41 myfile.txt
+ #-rw-r--r-- 1 user user 192 jan 8 14:39 spider.txt
+ 
