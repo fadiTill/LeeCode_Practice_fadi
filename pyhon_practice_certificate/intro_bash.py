a59d0078c803f16dbab2296e75663b5792ef986a
Fadimata Tillman <fadi.tillman@gmail.com>
2022-05-20 10:42:48 -0700

    update

--- pyhon_practice_certificate/intro_bash.py
+++ pyhon_practice_certificate/intro_bash.py
@@ -13,4 +13,7 @@
  # touch  myfile.txt-> create empty file 
  #ls -l -> total 4 -> -rw-r--r 1 user O Jan 8 14:41 myfile.txt
  #-rw-r--r-- 1 user user 192 jan 8 14:39 spider.txt
- 
+
+ #ls -la -> show iden files 
+ # mv -> move or rename file ->  mv myfile.txt emptyfile.txt
+ # cp spider.txt yetanotherfile.txt
